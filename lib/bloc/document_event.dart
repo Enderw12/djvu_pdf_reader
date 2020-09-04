980166d3129c88fdfdf21d22f004abc38b589478
Enderw12 <enderw2012@gmail.com>
2020-09-04 21:34:59 +0300

    больше комментариев

--- lib/bloc/document_event.dart
+++ lib/bloc/document_event.dart
@@ -2,10 +2,12 @@ part of 'document_bloc.dart';
 
 @immutable
 abstract class DocumentEvent {
+  /// базовый класс для всех возможных событий относящихся к данному блоку   
   const DocumentEvent();
 }
 
 class OpenDocument extends DocumentEvent {
+  /// данный ивент (он же событие) может содержать в себе любые данные в ссловаре (Map) data, в самом блоке получаем доступ по ключу.
   final Map data;
   const OpenDocument(this.data);
 }
