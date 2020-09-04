067a326905ae306d09a87a2b7db78a7094f161f6
Enderw12 <enderw2012@gmail.com>
2020-09-04 21:30:39 +0300

    добавлены комментарии

--- lib/widgets/alert_dialog.dart
+++ lib/widgets/alert_dialog.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> showExitDialog(ctx) async {
+  /// этот диалог вынесен в отдельный файл для демонстрации возможностей организации структуры приложения.
+  
   return showDialog<void>(
     context: ctx,
     barrierDismissible: false, // user must tap button!
     builder: (BuildContext context) {
       return AlertDialog(
+        // отображает диалоговое окно поверх остальных объектов на экране. 
         title: Text('Требуется подтверждение!'),
         content: SingleChildScrollView(
           child: ListBody(
@@ -20,6 +23,7 @@ Future<void> showExitDialog(ctx) async {
             padding: const EdgeInsets.all(8.0),
             child: FlatButton(
               child: Text('Выход'),
+              // SystemNavigator.pop() отдаёт команду на завершение работы приложения
               onPressed: () => SystemNavigator.pop(),
             ),
           ),
@@ -28,6 +32,7 @@ Future<void> showExitDialog(ctx) async {
             child: FlatButton(
               child: Text('Отмена'),
               onPressed: () {
+                // данный метод "закрывает" тот экран с которого он вызван. В данном случае таковым является виджет класса AlertDialog
                 Navigator.of(context).pop();
               },
             ),
