f5b0d0ab68a4866a3371a99392b777ba428aac0d
Enderw12 <enderw2012@gmail.com>
2020-09-04 22:18:31 +0300

    больше комментариев

--- lib/bloc/document_bloc.dart
+++ lib/bloc/document_bloc.dart
@@ -13,11 +13,13 @@ class DocumentBloc extends Bloc<DocumentEvent, DocumentState> {
   Stream<DocumentState> mapEventToState(
     DocumentEvent event,
   ) async* {
-    if (event is OpenDocument) {
+    if (event is OpenDocument)
       final data = {
         'filePath': event.data['filePath'],
         'fileName': event.data['fileName'],
       };
+      // Поскольку в "блоке" выбора файла совершены все необходимые проверки, тут возвращается единственное возможное состояние этого экрана, содержит имя документа и путь к нему.
+      // могут быть добавлены и другие обработки ивентов и преобразования в состояния в последующих if-else конструкциях.
       yield DocumentLoaded(data);
     }
   }
